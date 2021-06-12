5dbf03e2885464cc33313e8922bbae3196566db4
Avinash <Avinashkumarsinha007@gmail.com>
2021-06-12 16:14:24 +0530

    firebase

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,15 +1,22 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { DataListReducer } from "./DataList/reducer";
+import { fireReducer } from "../Components/Login/fireReducer";
+import {reactReduxFirebase ,getFirebase} from "react-redux-firebase";
+import firebaseConfig from "../Components/Login/firebaseConfig"; 
+import { firebaseReducer } from "react-redux-firebase";
 
 const rootReducer = combineReducers({
     dataList: DataListReducer,
+    fireReducer:fireReducer,
+    firebase :firebaseReducer,
 });
 
 export const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(thunk),
+        applyMiddleware(thunk.withExtraArgument({getFirebase})),
+        // reactReduxFirebase(),
         window.__REDUX_DEVTOOLS_EXTENSION__ &&
             window.__REDUX_DEVTOOLS_EXTENSION__(),
     ),
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,7 +4,7 @@ import { CustomDynamicPage } from "../Components/CustomDynamicPage/CustomDynamic
 import { DropDown } from "../Components/DropDown/DropDown";
 import { Footer } from "../Components/Footer/Footer";
 import { Home } from "../Components/HomePage/Home";
-import { NavBar } from "../Components/NavBar/NavBar";
+import  NavBar  from "../Components/NavBar/NavBar";
 const Routes = () => {
     return (
         <div>
