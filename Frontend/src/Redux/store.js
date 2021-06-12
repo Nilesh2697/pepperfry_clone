9e667b0b42707fffeae6fe371fd27198bad34947
Avinash <Avinashkumarsinha007@gmail.com>
2021-06-12 16:46:56 +0530

    new structure

--- Frontend/src/Redux/store.js
+++ Frontend/src/Redux/store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { DataListReducer } from "./DataList/reducer";
-<<<<<<< HEAD:src/Redux/store.js
 import { fireReducer } from "../Components/Login/fireReducer";
 import {reactReduxFirebase ,getFirebase} from "react-redux-firebase";
 import firebaseConfig from "../Components/Login/firebaseConfig"; 
@@ -11,13 +10,6 @@ const rootReducer = combineReducers({
     dataList: DataListReducer,
     fireReducer:fireReducer,
     firebase :firebaseReducer,
-=======
-import { SearchReducer } from "./Search/reducer";
-
-const rootReducer = combineReducers({
-    dataList: DataListReducer,
-    search: SearchReducer
->>>>>>> 48bec79aacb4dc07078157791381d3d775679fb1:Frontend/src/Redux/store.js
 });
 
 export const store = createStore(
