690276a8aa2b35303a60adf452a08049edb61d96
Vinesh <vinesh.nair31@gmail.com>
2021-06-14 08:32:56 +0530

    update

--- Frontend/src/Components/ProductPage/ProductPage.jsx
+++ Frontend/src/Components/ProductPage/ProductPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import styles from "styled-components"
+
+const ProductBox = styles.div`
+    width: 100%;
+    height: 500px;
+    border: 1px solid black;
+`
 
 const ProductPage = () => {
     return (
-        <div>
-            po
-        </div>
+        <ProductBox>
+
+        </ProductBox>
     )
 }
 
